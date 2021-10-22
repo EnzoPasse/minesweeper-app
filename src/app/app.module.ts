91104b860a7fbdecbb0e5df3a091fb1f71bcff8d
Enzo Passetti <enzo.passetti@outlook.com>
2021-10-22 11:35:23 -0300

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './components/game/game.component';
+import { BoardComponent } from './components/game/board/board.component';
+import { CellComponent } from './components/game/cell/cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    BoardComponent,
+    CellComponent
   ],
   imports: [
     BrowserModule,
