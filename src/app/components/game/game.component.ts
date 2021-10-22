acb432857a73ba780ce1b66a81276e753ac40ae0
Enzo Passetti <enzo.passetti@outlook.com>
2021-10-22 18:15:00 -0300

    game button reset, template: contextemenu, icons and more

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,19 +1,37 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Board } from './board/board.model';
+import { Cell } from './cell/cell.model';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent {
 
-board = new Board(5,5);
+  board!: Board;
 
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor() { this.initBoard() }
+
+
+  initBoard() {
+    this.board = new Board(4, 1);
+  }
+
+  checkCell(cell: Cell): void {
+    const result = this.board.checkCell(cell);
+    if (result === 'gameover') {
+      alert('GAME OVER');
+    } else if (result === 'win') {
+      alert('YOU WIN');
+    }
+  }
+
+  addFlag(cell: Cell): void {
+    this.board.addFlag(cell);
   }
 
 }
