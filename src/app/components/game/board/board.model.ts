99c3df4cf168e011b03b8d213c75b1cec847ae8e
Enzo Passetti <enzo.passetti@outlook.com>
2021-10-22 16:06:00 -0300

    checks clicked cell, and add flags

--- src/app/components/game/board/board.model.ts
+++ src/app/components/game/board/board.model.ts
@@ -2,18 +2,22 @@ import { AREA, Cell } from "../cell/cell.model"
 
 export class Board {
     cells: Cell[][] = [];
+    totalCells: number = 0;
 
-    constructor(size: number, mines: number) {
+    constructor(public size: number, mines: number) {
+
+        this.totalCells = (size * size) - mines
+
         for (let y = 0; y < size; y++) {
             this.cells[y] = [];
             for (let x = 0; x < size; x++) {
-                this.cells[y][x] = new Cell(y,x);
+                this.cells[y][x] = new Cell(y, x);
             }
 
         }
 
         this.plantMines(mines);
-        this.countMinesArround(size);
+        this.countMinesArround();
     }
 
     private plantMines(mines: number): void {
@@ -28,23 +32,56 @@ export class Board {
         return this.cells[y][x];
     }
 
-    private countMinesArround(size: number){
-   // recorro todo el tablero y en por cada posicion voy a preguntar por su area.
-      for (let y = 0; y < size; y++) {
-          for (let x = 0; x < size; x++) {
-              
-            let minesArround = 0;
-              
-             for (const item of AREA) {
-                 // que exista la celda y si tiene una mina la cuento.
-                 if(this.cells [y+item[0]] && this.cells[x+item[1]] && this.cells [y+item[0]] [x+item[1]].mine )  {
-                    minesArround ++
-                 }
-             }
-             this.cells[y][x].areaMines = minesArround
-             
-          }
-          
-      }
+    private countMinesArround() {
+        // recorro todo el tablero y en por cada posicion voy a preguntar por su area.
+        for (let y = 0; y < this.size; y++) {
+            for (let x = 0; x < this.size; x++) {
+
+                let minesArround = 0;
+
+                for (const item of AREA) {
+                    // que exista la celda y si tiene una mina la cuento.
+                    if (this.cells[y + item[0]] && this.cells[x + item[1]] && this.cells[y + item[0]][x + item[1]].mine) {
+                        minesArround++
+                    }
+                }
+                this.cells[y][x].areaMines = minesArround
+
+            }
+
+        }
+    }
+
+    private openAllCells():void {
+        for (let y = 0; y < this.size; y++) {
+            for (let x = 0; x < this.size; x++) {
+                this.cells[y][x].status = 'open';
+                
+            }
+            
+        }
+    }
+
+    public checkCell(cell: Cell): 'gameover' | 'win' | null {
+        if (cell.status !== 'open') {
+            return null;
+        }
+        if (cell.mine) {
+          //  this.openAllCells();
+            return 'gameover'
+        } else {
+            cell.status = 'clear';
+            if (this.totalCells-- <= 1)
+                return 'win';
+        }
+        return null;
+    }
+
+    public addFlag(cell: Cell): void {
+        if (cell.status === 'flag') {
+            cell.status = 'open';
+        } else {
+            cell.status = 'flag';
+        }
     }
 }
--- src/app/components/game/cell/cell.model.ts
+++ src/app/components/game/cell/cell.model.ts
@@ -6,12 +6,14 @@ export class Cell {
     constructor(public row: number, public column: number) { }
 }
 
+
+// son los 9 espacios alrededor de una celda en particular
 export const AREA = [
     [-1, -1],
     [-1, 0],
     [-1, 1],
     [0, -1],
-    [0, 0],
+    [0, 0], //  la celda en cuestion
     [0, 1],
     [1, -1],
     [1, 0],
