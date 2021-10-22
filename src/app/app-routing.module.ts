0d2aa67de1c6be3967caed214f2526104e8b736c
Enzo Passetti <enzo.passetti@outlook.com>
2021-10-22 11:35:03 -0300

    create  components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameComponent } from './components/game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo:'/juego'
+  },
+  
+  {
+    path:'juego',
+    component: GameComponent   
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
